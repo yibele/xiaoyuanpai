68798c1810d56d1880f36207e8619d8e1f7a9307
yibele <yibeel@163.com>
2017-07-03 19:09:27 +0800

    正在进行用户界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,5 +4,14 @@ Page({
     toView: 'red',
     scrollTop: 100,
     des: "狼人杀活动 \n 星期日 19:00"
+  },
+  toDetail : function () {
+    wx.navigateTo({
+      url: '/pages/detail/detail',
+      success: function(res) {
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
   }
 })
