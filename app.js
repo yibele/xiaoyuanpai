932219e0f0ea87d9085fc8a6562e166571c8a2c5
yibele <yibeel@163.com>
2017-07-04 12:22:29 +0800

    初步完成首页数据，有一个小bug存在，就是有时候app.js 中的globalData 无法设置导致index.js 中取不到userData  以后修改

--- app.js
+++ app.js
@@ -1,86 +1,78 @@
 //app.js
+/**
+ * 这里我们做了两件事情
+ * 1 事情是从数据库中获取用户信息，如果用户
+ * 信息不存在，就插入用户信息。
+ * 2 将获取的用户信息存在全局变量userData中，其中包括用户信息和用户参加的互动信息as
+ */
 App({
-  onLaunch: function() {
-    //调用API从本地缓存中获取数据
-    var that = this;
-    //检测用户登陆
-    wx.login({
-      success : function (res) {
-        if(res.code) {
-          //获取用户信息
-          wx.getUserInfo({
-            success : function(res) {
-              //将用户信息保存在全局变量中
-              that.globalData.userInfo = res.userInfo;
-              wx.request({
-                url: 'http://localhost:8888/v1/user/find/'+res.userInfo.nickName,
-                success: function (res) {
-                  if(res.data.code === 40001) {
-                    wx.request({
-                      url: 'http://localhost:8888/v1/user/',
-                      method: 'POST',
-                      header : {
-                        'content-type': 'application/x-www-form-urlencoded'
-                      },
-                      data: {
-                        nickName: res.userInfo.nickName,
-                        avatarUrl: res.userInfo.avatarUrl,
-                        phone: "",
-                        gender:res.userInfo.gender
-                      },
-                      success: function (res1) {
-                        if(res1.code === 10001) {
-                          wx.request({
-                            url: 'http://localhost:8888/v1/user/find/'+res.userInfo.nickName,
-                            success : function (res2) {
-                              wx.setStorage({
-                                key: 'user',
-                                data: 'res2.data.data',
-                                success : function () {
-                                  console.log('add new user success');
+    onLaunch: function() {
+        //调用API从本地缓存中获取数据
+        var that = this;
+        //检测用户登陆
+        wx.login({
+            success: function(res) {
+                if (res.code) {
+                    //获取用户信息
+                    wx.getUserInfo({
+                        success: function(res) {
+                            //将用户信息保存在全局变量中
+                            that.globalData.userInfo = res.userInfo;
+                            wx.request({
+                                url: 'http://localhost:8888/v1/user/find/' + res.userInfo.nickName,
+                                success: function(res3) {
+                                    if (res3.data.code === 40001) {
+                                        wx.request({
+                                            url: 'http://localhost:8888/v1/user/',
+                                            method: 'POST',
+                                            header: {
+                                                'content-type': 'application/x-www-form-urlencoded'
+                                            },
+                                            data: {
+                                                nickName: res.userInfo.nickName,
+                                                avatarUrl: res.userInfo.avatarUrl,
+                                                phone: "",
+                                                gender: res.userInfo.gender
+                                            },
+                                            success: function(res1) {
+                                                if (res1.code === 10001) {
+                                                    wx.request({
+                                                        url: 'http://localhost:8888/v1/user/find/' + res.userInfo.nickName,
+                                                        success: function(res2) {
+                                                            that.globalData.userData = res2.data.data;
+                                                        }
+                                                    })
+                                                }
+                                            }
+                                        })
+                                    } else {
+                                        //将用户信息储存在 ’user' 键中
+                                        /**
+                                        wx.setStorage({
+                                          key: 'user',
+                                          data: res.data.data,
+                                          success: function () {
+                                            var tmp = wx.getStorageSync('user');
+                                            console.log(tmp);
+                                          }
+                                        })
+                                        */
+                                        that.globalData.userData = res3.data.data;
+                                    }
                                 }
-                              })
-                            }
-                          })
-                        }              
-                      }
+                            })
+                        }
                     })
-                    console.log(res.data.code);
-                  } else {
-                    //将用户信息储存在 ’user' 键中
-                    wx.setStorage({
-                      key: 'user',
-                      data: res.data.data,
-                      success: function () {
-                        var tmp = wx.getStorageSync('user');
-                        console.log(tmp);
-                      }
-                    })
-                  }
                 }
-              })
             }
-          })
-        }
-      }
-    })
-  },
-  getUserInfo: function(cb) {
-    var that = this
-    if (this.globalData.userInfo) {
-      typeof cb == "function" && cb(this.globalData.userInfo)
-    } else {
-      //调用登录接口
-      wx.getUserInfo({
-        withCredentials: false,
-        success: function(res) {
-          that.globalData.userInfo = res.userInfo
-          typeof cb == "function" && cb(that.globalData.userInfo)
-        }
-      })
+        })
+    },
+    getUserData: function() {
+        return this.globalData.userData;
+
+    },
+    globalData: {
+        userInfo: null,
+        userData: null,
     }
-  },
-  globalData: {
-    userInfo: null
-  }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,40 +1,73 @@
-var app = getApp();
+
 Page({
-  data: {
-    toView: 'red',
-    scrollTop: 100,
-    des: "狼人杀活动 \n 星期日 19:00",
-    user : {},
-    user_act : [],
-    act_count : ''
-  },
-  onLoad : function () {
-    //获取用户数据和活动信息
-    this.getUser();
-  },
-  toDetail : function () {
-    wx.navigateTo({
-      url: '/pages/detail/detail',
-      success: function(res) {
-      },
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  },
-  getUser: function () {
-    var that = this;
-    wx.getStorage({
-      key: 'user',
-      success: function(res) {
-        console.log(res.data);
-        that.setData({
-          user :res.data.user,
-          user_act : res.data.act,
-          act_count : res.data.actCount
+    data: {
+        toView: 'red',
+        scrollTop: 100,
+        des: "狼人杀活动 \n 星期日 19:00",
+        user: {},
+        user_act: null,
+        act_count: '',
+        index_data: {}
+    },
+    onLoad: function() {
+        //获取用户数据和活动信息
+        this.getUser();
+        this.getActivity();
+    },
+    toDetail: function() {
+        wx.navigateTo({
+            url: '/pages/detail/detail',
+            success: function(res) {},
+            fail: function(res) {},
+            complete: function(res) {},
         })
-      },
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-  }
+    },
+    //获取用户信息，并将用户信息存入变量当中
+    getUser: function() {
+        var that = this;
+        var app = getApp();
+        var userData = app.getUserData();
+        if(userData != null) {
+            that.setData({
+                user: userData.user,
+                act_count: userData.actCount,
+                user_act: userData.act
+            });
+        } else {
+
+        }
+        /**
+        wx.getStorage({
+          key: 'user',
+          success: function(res) {
+              console.log(res);
+            that.setData({
+              user :res.data.user,
+              user_act : res.data.act,
+              act_count : res.data.actCount
+            })
+            console.log('set index data success');
+          },
+          fail: function(res) {
+              console.log('get Data fail');
+          },
+          complete: function(res) {},
+        })
+        */
+    },
+    //获取首页活动信息
+    getActivity: function() {
+        var that = this
+        wx.request({
+            url: 'http://localhost:8888/v1/activity/index',
+            success: function(res) {
+                console.log(res.data);
+                if (res.data.code == 10004) {
+                    that.setData({
+                        index_data: res.data.data
+                    })
+                }
+            }
+        })
+    }
 })
